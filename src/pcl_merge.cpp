837939ba237511bb0006869e137f5a5d9c165684
Gene Rush <erush91@gmail.com>
2020-02-05 23:09:47 -0700

    Achieved 10Hz performance using 95-100% of a core. Code is not flexible, the merged pcl is published in the frame of the first sensor, in this case os1_lidar.

--- src/pcl_merge.cpp
+++ src/pcl_merge.cpp
@@ -21,7 +21,6 @@ using namespace sensor_msgs;
 using namespace message_filters;
 
 int maximum_queue_size_;
-std::string output_frame_;
 bool approximate_sync_;
 
 ros::Publisher pub_output_;
@@ -29,83 +28,63 @@ ros::Publisher pub_output_;
 long int cnt = 0;
 
 // Reference Frames
-std::string robot_frame_;
-std::string ouster_frame_;
-std::string up_pico_flexx_frame_;
-std::string down_pico_flexx_frame_;
+std::string output_frame_;
+std::string in2_frame_;
+std::string in3_frame_;
 
-// TF Listener
-boost::shared_ptr<tf::TransformListener> ouster_listener;
-tf::StampedTransform ouster_transform;
-tf::Matrix3x3 ouster_m_euler;
-tf::Matrix3x3 ouster_m_inv_euler;
-double ouster_roll;
-double ouster_pitch;
-double ouster_yaw;
-
-boost::shared_ptr<tf::TransformListener> up_pico_flexx_listener;
-tf::StampedTransform up_pico_flexx_transform;
-tf::Matrix3x3 up_pico_flexx_m_euler;
+boost::shared_ptr<tf::TransformListener> tf_2_listener;
+tf::StampedTransform tf_2;
+tf::Matrix3x3 m_euler2;
 tf::Matrix3x3 up_pico_flexx_m_inv_euler;
-double up_pico_flexx_roll;
-double up_pico_flexx_pitch;
-double up_pico_flexx_yaw;
+double roll2;
+double pitch2;
+double yaw2;
 
-boost::shared_ptr<tf::TransformListener> down_pico_flexx_listener;
-tf::StampedTransform down_pico_flexx_transform;
-tf::Matrix3x3 down_pico_flexx_m_euler;
+boost::shared_ptr<tf::TransformListener> tf_3_listener;
+tf::StampedTransform tf_3;
+tf::Matrix3x3 m_euler3;
 tf::Matrix3x3 down_pico_flexx_m_inv_euler;
-double down_pico_flexx_roll;
-double down_pico_flexx_pitch;
-double down_pico_flexx_yaw;
+double roll3;
+double pitch3;
+double yaw3;
 
 
-void callback(const PointCloud2::ConstPtr &cloud_ouster_ros, const PointCloud2::ConstPtr &cloud_up_pico_flexx_ros, const PointCloud2::ConstPtr &cloud_down_pico_flexx_ros)
+void callback(const PointCloud2::ConstPtr &in1_ros, const PointCloud2::ConstPtr &in2_ros, const PointCloud2::ConstPtr &in3_ros)
 {
+
+
+  pcl::PointCloud<pcl::PointXYZI> in1;
+  pcl::fromROSMsg(*in1_ros, in1);
+
+  pcl::PointCloud<pcl::PointXYZI> in2;
+  pcl::fromROSMsg(*in2_ros, in2);
+
+  pcl::PointCloud<pcl::PointXYZI> in3;
+  pcl::fromROSMsg(*in3_ros, in3);
+
+  pcl::PointCloud<pcl::PointXYZI> in2_t;
+  pcl::PointCloud<pcl::PointXYZI> in3_t;
+
+  pcl_ros::transformPointCloud (in2, in2_t, tf_2);
+  pcl_ros::transformPointCloud (in3, in3_t, tf_3);
 
   std::cout << "Entering callback" << std::endl;
 
-
-  pcl::PointCloud<pcl::PointXYZI> cloud_ouster;
-  pcl::fromROSMsg(*cloud_ouster_ros, cloud_ouster);
-
-  pcl::PointCloud<pcl::PointXYZI> cloud_up_pico_flexx;
-  pcl::fromROSMsg(*cloud_up_pico_flexx_ros, cloud_up_pico_flexx);
-
-  pcl::PointCloud<pcl::PointXYZI> cloud_down_pico_flexx;
-  pcl::fromROSMsg(*cloud_down_pico_flexx_ros, cloud_down_pico_flexx);
-
-  pcl::PointCloud<pcl::PointXYZI>::Ptr cloud_merge (new pcl::PointCloud<pcl::PointXYZI>);
-
-  pcl::PointCloud<pcl::PointXYZI> cloud_ouster_xfrm;
-  pcl_ros::transformPointCloud(cloud_ouster, cloud_ouster_xfrm, ouster_transform);
-
-  pcl::PointCloud<pcl::PointXYZI> cloud_up_pico_flexx_xfrm;
-  pcl_ros::transformPointCloud(cloud_up_pico_flexx, cloud_up_pico_flexx_xfrm, up_pico_flexx_transform);
-
-  pcl::PointCloud<pcl::PointXYZI> cloud_down_pico_flexx_xfrm;
-  pcl_ros::transformPointCloud(cloud_down_pico_flexx, cloud_down_pico_flexx_xfrm, down_pico_flexx_transform);
-
-  for (int i = 0; i < cloud_ouster_xfrm.points.size(); i++)
+  pcl::PointCloud<pcl::PointXYZI>::Ptr out (new pcl::PointCloud<pcl::PointXYZI>);
+  for (int i = 0; i < in2_t.points.size(); i++)
   {
-      cloud_merge->push_back(cloud_ouster_xfrm.points[i]);
+      in1.push_back(in2_t.points[i]);
+  }
+  for (int i = 0; i < in3_t.points.size(); i++)
+  {
+      in1.push_back(in3_t.points[i]);
   }
 
-  for (int i = 0; i < cloud_up_pico_flexx_xfrm.points.size(); i++)
-  {
-      cloud_merge->push_back(cloud_up_pico_flexx_xfrm.points[i]);
-  }
-  
-  for (int i = 0; i < cloud_down_pico_flexx_xfrm.points.size(); i++)
-  {
-      cloud_merge->push_back(cloud_down_pico_flexx_xfrm.points[i]);
-  }
-          
   sensor_msgs::PointCloud2 cloud_merge_ros;
-  pcl::toROSMsg(*cloud_merge, cloud_merge_ros);
+  pcl::toROSMsg(in1, cloud_merge_ros);
 
   cloud_merge_ros.header.stamp = ros::Time::now();
-  cloud_merge_ros.header.frame_id = "base_link";
+  cloud_merge_ros.header.frame_id = output_frame_;
 
   pub_output_.publish (boost::make_shared<PointCloud2> (cloud_merge_ros));
 
@@ -116,9 +95,8 @@ void callback(const PointCloud2::ConstPtr &cloud_ouster_ros, const PointCloud2::
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "pcl_merge");
-  ouster_listener.reset(new tf::TransformListener);
-  down_pico_flexx_listener.reset(new tf::TransformListener);
-  up_pico_flexx_listener.reset(new tf::TransformListener);
+  tf_3_listener.reset(new tf::TransformListener);
+  tf_2_listener.reset(new tf::TransformListener);
 
   ros::NodeHandle nh;
   ros::NodeHandle private_nh("~");
@@ -126,47 +104,31 @@ int main(int argc, char** argv)
   private_nh.getParam ("max_queue_size", maximum_queue_size_);
   private_nh.getParam ("approximate_sync", approximate_sync_);
   private_nh.getParam ("output_frame", output_frame_);
-  private_nh.getParam ("robot_frame", robot_frame_);
-  private_nh.getParam ("ouster_frame", ouster_frame_);
-  private_nh.getParam ("up_pico_flexx_frame", up_pico_flexx_frame_);
-  private_nh.getParam ("down_pico_flexx_frame", down_pico_flexx_frame_);
+  private_nh.getParam ("in2_frame", in2_frame_);
+  private_nh.getParam ("in3_frame", in3_frame_);
 
   // Method to get the most recent (and most synchronized) messages from the three sensors below
     // http://wiki.ros.org/message_filters/ApproximateTime
     // http://wiki.ros.org/message_filters
-  message_filters::Subscriber<PointCloud2> ouster_sub(nh, "os1_cloud_node/points", 5);
-  message_filters::Subscriber<PointCloud2> up_pico_flexx_sub(nh, "up/up_camera/stream/1/cloud", 5);
-  message_filters::Subscriber<PointCloud2> down_pico_flexx_sub(nh, "down/down_camera/stream/1/cloud", 5);
+  message_filters::Subscriber<PointCloud2> ouster_sub(nh, "in1", 5);
+  message_filters::Subscriber<PointCloud2> up_pico_flexx_sub(nh, "in2", 5);
+  message_filters::Subscriber<PointCloud2> down_pico_flexx_sub(nh, "in3", 5);
 
   typedef sync_policies::ApproximateTime<PointCloud2, PointCloud2, PointCloud2> MySyncPolicy;
   // ApproximateTime takes a queue size as its constructor argument, hence MySyncPolicy(10)
   Synchronizer<MySyncPolicy> sync(MySyncPolicy(10), ouster_sub, up_pico_flexx_sub, down_pico_flexx_sub);
   sync.registerCallback(boost::bind(&callback, _1, _2, _3));
 
-  pub_output_ = private_nh.advertise<PointCloud2> ("output", maximum_queue_size_);
+  pub_output_ = private_nh.advertise<PointCloud2> ("cloud", maximum_queue_size_);
   
-  // Get Ouster static transforms     
-  try
-  {
-      ouster_listener->waitForTransform(robot_frame_, ouster_frame_, ros::Time(0), ros::Duration(10.0) );
-      ouster_listener->lookupTransform(robot_frame_, ouster_frame_, ros::Time(0), ouster_transform);
-      ouster_m_euler.setRotation(ouster_transform.getRotation());
-      int ouster_solution_number = 1;
-      ouster_m_euler.getEulerYPR(ouster_yaw, ouster_pitch, ouster_roll, ouster_solution_number);
-  }
-  catch (tf::TransformException ex)
-  {
-      ROS_WARN("%s",ex.what());
-  }
-
   // Get Up Pico Flexx static transforms     
   try
   {
-      up_pico_flexx_listener->waitForTransform(robot_frame_, up_pico_flexx_frame_, ros::Time(0), ros::Duration(10.0) );
-      up_pico_flexx_listener->lookupTransform(robot_frame_, up_pico_flexx_frame_, ros::Time(0), up_pico_flexx_transform);
-      up_pico_flexx_m_euler.setRotation(up_pico_flexx_transform.getRotation());
-      int up_pico_flexx_solution_number = 1;
-      up_pico_flexx_m_euler.getEulerYPR(up_pico_flexx_yaw, up_pico_flexx_pitch, up_pico_flexx_roll, up_pico_flexx_solution_number);
+      tf_2_listener->waitForTransform(output_frame_, in2_frame_, ros::Time(0), ros::Duration(10.0) );
+      tf_2_listener->lookupTransform(output_frame_, in2_frame_, ros::Time(0), tf_2);
+      m_euler2.setRotation(tf_2.getRotation());
+      int sol_num2 = 1;
+      m_euler2.getEulerYPR(yaw2, pitch2, roll2, sol_num2);
   }
   catch (tf::TransformException ex)
   {
@@ -176,11 +138,11 @@ int main(int argc, char** argv)
   // Get Down Pico Flexx static transforms     
   try
   {
-      down_pico_flexx_listener->waitForTransform(robot_frame_, down_pico_flexx_frame_, ros::Time(0), ros::Duration(10.0) );
-      down_pico_flexx_listener->lookupTransform(robot_frame_, down_pico_flexx_frame_, ros::Time(0), down_pico_flexx_transform);
-      down_pico_flexx_m_euler.setRotation(down_pico_flexx_transform.getRotation());
-      int down_pico_flexx_solution_number = 1;
-      down_pico_flexx_m_euler.getEulerYPR(down_pico_flexx_yaw, down_pico_flexx_pitch, down_pico_flexx_roll, down_pico_flexx_solution_number);
+      tf_3_listener->waitForTransform(output_frame_, in3_frame_, ros::Time(0), ros::Duration(10.0) );
+      tf_3_listener->lookupTransform(output_frame_, in3_frame_, ros::Time(0), tf_3);
+      m_euler3.setRotation(tf_3.getRotation());
+      int sol_num3 = 1;
+      m_euler3.getEulerYPR(yaw3, pitch3, roll3, sol_num3);
   }
   catch (tf::TransformException ex)
   {
